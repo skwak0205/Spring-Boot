33bda409d677406560a508723a31209a154b59fa
Soojung Kwak <skwak0205@gmail.com>
2021-06-04 14:16:43 +0900

    페이징 처리 & 검색 기능 구현

--- StartBoot06/src/main/java/com/boot/tutorial/controller/BoardController.java
+++ StartBoot06/src/main/java/com/boot/tutorial/controller/BoardController.java
@@ -1,10 +1,11 @@
 package com.boot.tutorial.controller;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -28,9 +29,19 @@ public class BoardController {
 	private BoardValidator boardValidator;
 
 	@GetMapping("/list")
-	public String list(Model model) {
-		List<Board> boards = boardRepository.findAll();
+	public String list(Model model, @PageableDefault(size = 2) Pageable pageable,
+			@RequestParam(required = false, defaultValue = "") String searchText) {
+
+//		Page<Board> boards = boardRepository.findAll(pageable);
+		Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+
+		int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+		int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+
+		model.addAttribute("startPage", startPage);
+		model.addAttribute("endPage", endPage);
 		model.addAttribute("boards", boards);
+
 		return "board/list";
 	}
 
@@ -46,7 +57,7 @@ public class BoardController {
 	}
 
 	@PostMapping("/form")
-	public String greetingSubmit(@Valid Board board, BindingResult bindingResult) {
+	public String insertForm(@Valid Board board, BindingResult bindingResult) {
 		boardValidator.validate(board, bindingResult);
 
 		if (bindingResult.hasErrors()) {
