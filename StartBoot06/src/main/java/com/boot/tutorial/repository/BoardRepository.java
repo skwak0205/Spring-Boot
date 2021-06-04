d7e2873676ea1239b52d24f6d46fcbfca53219cd
Soojung Kwak <skwak0205@gmail.com>
2021-06-04 14:17:29 +0900

    페이징 처리 & 검색 기능 구현

--- StartBoot06/src/main/java/com/boot/tutorial/repository/BoardRepository.java
+++ StartBoot06/src/main/java/com/boot/tutorial/repository/BoardRepository.java
@@ -2,6 +2,8 @@ package com.boot.tutorial.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.boot.tutorial.model.Board;
@@ -10,5 +12,8 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 	List<Board> findByTitle(String title);
 
 	List<Board> findByTitleOrContent(String title, String content);
+
+	// 검색
+	Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 
 }
