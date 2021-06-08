ed39103d314be4bc732e0b37a97fadb572944bf6
Soojung Kwak <skwak0205@gmail.com>
2021-06-08 15:18:37 +0900

    simple Restful API

---
+++ StartBoot07/src/main/java/com/example/startboot07/StartBoot07Application.java
@@ -0,0 +1,13 @@
+package com.example.startboot07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartBoot07Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartBoot07Application.class, args);
+    }
+
+}
